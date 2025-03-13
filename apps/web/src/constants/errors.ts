75cb086e147a63eb54e212dad080a29a6c7acdb9
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2025-03-13 23:09:15 +0200

    chore: remove extra consts

--- apps/web/src/constants/errors.ts
+++
@@ -1,4 +0,0 @@
-export const ERROR = {
-  unexpected: "Something went wrong.Try again.",
-  cantDeleteTank: "You could not delete this tank.",
-};
--- apps/web/src/constants/roles.ts
+++
@@ -1,4 +0,0 @@
-export const ROLE = {
-  admin: "admin",
-  user: "user"
-}
